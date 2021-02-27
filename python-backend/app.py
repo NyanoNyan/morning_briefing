855205f03672941039b2d99a080497cce4211fc6
Lackshan <lackshan.rama@gmail.com>
2021-02-27 23:00:14 +0000

    Updated api-spec and created app.py placeholder

---
+++ python-backend/app.py
@@ -0,0 +1,24 @@
+@app.route('/')
+def index():
+    return "Welcome to Morning Briefing"
+
+@app.route('/login')
+
+
+
+weather data:
+
+convert current into hourly
+
+"dt": 1586390400,
+"temp": 278.41,
+"feels_like": 269.43,
+"humidity": 65,
+"wind_speed": 9.83,
+"weather": [
+          {
+            "id": 800,
+            "main": "Clear",
+            "description": "clear sky"
+          }
+        ]
